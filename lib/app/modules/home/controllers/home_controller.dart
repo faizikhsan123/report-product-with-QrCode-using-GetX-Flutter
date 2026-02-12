66018bccb75d89ccef988eda1ba8b88d6aa31655
unknown <faizashary19@gmail.com>
2026-02-12 23:13:54 +0700

    fixing data in report pdf

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,131 +1,144 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf/pdf.dart'; //import package pdf
-import 'package:pdf/widgets.dart' as pw; //import widget pdf pw = pdf widget
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:qrcode_getx/app/data/models/product_model.dart';
 
 class HomeController extends GetxController {
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  RxList<Product> products = <Product>[].obs; //menggunakan RxList untuk products yg tipenya model Product awalnya daatanya kosong
+
+ 
   void DownloadKataLog() async {
-    final pdf = pw.Document(); //membuat objek dokumen PDF baru di Flutter.
+    final pdf = pw.Document();
+
+    var getData = await firestore.collection("products").get(); //ambil semua document ygg ada di collection products
+
+    //reset all data (modelsnya) agar tidak menjadi duplikat
+    products([]);
+
+    getData.docs.forEach((element) { //untuk mengambil data tiap document
+      products.add(Product.fromJson(element.data())); //masukkan data ke models products yg diatas(kosong) 
+    },);
 
     pdf.addPage(
-      //menambahkan halaman ke dokumen
-      pw.Page(
-        pageFormat: PdfPageFormat.a4, //format halaman A4
-        build: (context) => pw.Column(
-          children: [
-            pw.Center(
-              child: pw.Text(
-                "KataLog Products",
-                textAlign: pw.TextAlign.center,
-                style: pw.TextStyle(
-                  fontSize: 20,
-                  fontWeight: pw.FontWeight.bold,
-                ),
-              ),
+      pw.MultiPage(
+        //ganti ke pw.MultiPage karena pdfnya bisa lebih dari 1 halaman
+        pageFormat: PdfPageFormat.a4,
+        build: (context) => [ //multi page langsung return list
+
+          pw.Center(
+            child: pw.Text(
+              "KataLog Products",
+              textAlign: pw.TextAlign.center,
+              style: pw.TextStyle(fontSize: 20, fontWeight: pw.FontWeight.bold),
             ),
-            pw.SizedBox(height: 20),
-            pw.Table(
-              //widget table
-              border: pw.TableBorder.all(color: PdfColors.black, width: 2),
-              children: [ //list utamanya
-                pw.TableRow(
-                  //untuk judul nya table
+          ),
+          pw.SizedBox(height: 20),
+          
+          pw.Table(
+            border: pw.TableBorder.all(color: PdfColors.black, width: 2),
+            children: [
+              pw.TableRow(
+                children: [
+                  pw.Padding(
+                    padding: pw.EdgeInsets.all(10),
+                    child: pw.Text(
+                      'No',
+                      textAlign: pw.TextAlign.center,
+                      style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: pw.EdgeInsets.all(10),
+                    child: pw.Text(
+                      'Code',
+                      textAlign: pw.TextAlign.center,
+                      style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: pw.EdgeInsets.all(10),
+                    child: pw.Text(
+                      'Name',
+                      textAlign: pw.TextAlign.center,
+                      style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: pw.EdgeInsets.all(10),
+                    child: pw.Text(
+                      'Quantity',
+                      textAlign: pw.TextAlign.center,
+                      style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: pw.EdgeInsets.all(10),
+                    child: pw.Text(
+                      'QrCode',
+                      textAlign: pw.TextAlign.center,
+                      style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+
+              ...List.generate(
+                products.length, //generate sebanyak jumlah products
+                (index) => pw.TableRow(
+                  
                   children: [
                     pw.Padding(
                       padding: pw.EdgeInsets.all(10),
-                      child: pw.Text(
-                        'No',
-                        textAlign: pw.TextAlign.center,
-                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
-                      ),
+                      child: pw.Center(child: pw.Text('${index + 1}',),)
                     ),
                     pw.Padding(
                       padding: pw.EdgeInsets.all(10),
-                      child: pw.Text(
-                        'Code',
-                        textAlign: pw.TextAlign.center,
-                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
-                      ),
+                      child: pw.Center(child: pw.Text('${products[index].code}',),) //sekarang ambil dari models products
                     ),
                     pw.Padding(
                       padding: pw.EdgeInsets.all(10),
-                      child: pw.Text(
-                        'Name',
-                        textAlign: pw.TextAlign.center,
-                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
-                      ),
+                       child: pw.Center(child: pw.Text('${products[index].name}',),)
                     ),
                     pw.Padding(
                       padding: pw.EdgeInsets.all(10),
-                      child: pw.Text(
-                        'Quantity',
-                        textAlign: pw.TextAlign.center,
-                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
-                      ),
+                      child: pw.Center(child: pw.Text('${products[index].quantity}',),)
                     ),
                     pw.Padding(
                       padding: pw.EdgeInsets.all(10),
-                      child: pw.Text(
-                        'QrCode',
-                        textAlign: pw.TextAlign.center,
-                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                      child: pw.BarcodeWidget(
+                        //widget untuk membuat qr code
+                        data: '${products[index].code}', //buat barcode berdasarkan code
+                        barcode: pw.Barcode.qrCode(), //tampilan barcode
+                        height: 50,
+                        width: 50,
                       ),
                     ),
                   ],
                 ),
-
-                //isi datanya pakai list generate
-                ...List.generate( //arti ... adalah  Menyebarkan isi list ke dalam list lain (ini list anaknya dari list utama)
-                  3, //item count
-                  (index) => pw.TableRow(
-                    children: [
-                      pw.Padding(
-                        padding: pw.EdgeInsets.all(10),
-                        child: pw.Text('${index + 1}'),
-                      ),
-                      pw.Padding(
-                        padding: pw.EdgeInsets.all(10),
-                        child: pw.Text('12345678'),
-                      ),
-                      pw.Padding(
-                        padding: pw.EdgeInsets.all(10),
-                        child: pw.Text('Product'),
-                      ),
-                      pw.Padding(
-                        padding: pw.EdgeInsets.all(10),
-                        child: pw.Text('65'),
-                      ),
-                      pw.Padding(
-                        padding: pw.EdgeInsets.all(10),
-                        child: pw.Text('qrcode'),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
 
-    //simpan
     Uint8List bytes = await pdf.save();
 
-    //mendapatkan direktori aplikasi
     final dir = await getApplicationDocumentsDirectory();
 
-    //buat file kosong
     final file = File('${dir.path}/example.pdf');
 
-    //masukkan data yg ada di bytes ke file kosong tadi
     await file.writeAsBytes(bytes);
 
-    //open pdf
     await OpenFile.open(file.path);
   }
 }
